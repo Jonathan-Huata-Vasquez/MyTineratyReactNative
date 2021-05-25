9e38ae5509e92a5e2c32b609e31d7a61cb629ede
Jonathan Huata Vasquez <jonathan_huata@outlook.com>
2021-05-25 06:56:25 -0300

    correciones en estilos con el scrollView

--- pages/Itinerary.js
+++ pages/Itinerary.js
@@ -1,108 +1,122 @@
 import React from 'react';
-import {useState} from 'react'
-import { Text, View, StyleSheet, ScrollView,Image } from 'react-native'
-import { Avatar, FAB, IconButton,Chip,TouchableRipple } from 'react-native-paper'
+import { useState } from 'react'
+import { Text, View, StyleSheet, ScrollView, Image } from 'react-native'
+import { Avatar, FAB, IconButton, Chip, TouchableRipple } from 'react-native-paper'
 import { myContainer } from '../helpers/myStyles'
 import ImageView from "react-native-image-viewing";
 
 const Itinerary = (props) => {
     const [stateImageView, setStateImageView] = useState({
-        visible:false,
-        index:0
+        visible: false,
+        index: 0
     });
     let itinerary = props.route.params.itinerary
-    const images = itinerary.activities.map(activity=>({uri:activity.imagenHost}))
-        
-    
-    
+    const images = itinerary.activities.map(activity => ({ uri: activity.imagenHost }))
+
+
+
     return (
-        <View style={myContainer.body}>
-            <View style={styles.infoItinerary}>
-                <Avatar.Image size={85} source={{ uri: itinerary.autorFotoHost }} style={{ width: 85, height: 85, marginRight: 30 }} />
-                <View>
-                    <Text style={styles.title}>{itinerary.titulo}</Text>
-                    <Text style={styles.author}>by: {itinerary.autorNombre}</Text>
-                </View>
-            </View>
-            <View style={{ flexDirection: "row" ,justifyContent:"space-around",width:"100%",marginTop:10}}>
-                <View>
-                    <View style={styles.iconContainer}>
-                        <Text style={styles.amount}>{itinerary.likes} </Text>
-                        <IconButton
-                            icon={itinerary.estaLikeado ? "cards-heart" : "heart-outline"}
-                            color="#f50057"
-                            size={23}
-                            style={{ margin: 0, }}
-                            onPress={() => console.log('Pressed')}
-                        />
+        <ScrollView style={{ height: "90%", width: "100%" }}>
+            <View style={[myContainer.body,]}>
+                <View style={styles.infoItinerary}>
+                    <View style={{width:"25%"}}>
+                        <Avatar.Image size={85} source={{ uri: itinerary.autorFotoHost }} style={{ width: 85, height: 85, }} />
                     </View>
-                    <Text style={styles.textInfoIcon}>Likes</Text>
-                </View>
-                <View>
-                    <View style={styles.iconContainer}>
-                        <Text style={styles.amount}>{itinerary.precio} </Text>
-                        <IconButton
-                            icon="cash"
-                            color="#22bd22"
-                            size={23}
-                            style={{ margin: 0, }}
-                            onPress={() => console.log('Pressed')}
-                        />
+                    <View style={{marginLeft:20,width:"70%"}}>
+                        <Text style={styles.title}>{itinerary.titulo}</Text>
+                        <Text style={styles.author}>by: {itinerary.autorNombre}</Text>
                     </View>
-                    <Text style={styles.textInfoIcon}>Price</Text>
+
                 </View>
-                <View>
-                    <View style={styles.iconContainer}>
-                        <Text style={styles.amount}>{itinerary.duracion} </Text>
-                        <IconButton
-                            icon="clock-time-four-outline"
-                            color="rgb(72,144,226)"
-                            size={23}
-                            style={{ margin: 0, }}
-                        />
+                <View style={{ flexDirection: "row", justifyContent: "space-around", width: "100%", marginTop: 10 }}>
+                    <View>
+                        <View style={styles.iconContainer}>
+                            <Text style={styles.amount}>{itinerary.likes} </Text>
+                            <IconButton
+                                icon={itinerary.estaLikeado ? "cards-heart" : "heart-outline"}
+                                color="#f50057"
+                                size={23}
+                                style={{ margin: 0, }}
+                                onPress={() => console.log('Pressed')}
+                            />
+                        </View>
+                        <Text style={styles.textInfoIcon}>Likes</Text>
                     </View>
-                    <Text style={styles.textInfoIcon}>Duration</Text>
+                    <View>
+                        <View style={styles.iconContainer}>
+                            <Text style={styles.amount}>{itinerary.precio} </Text>
+                            <IconButton
+                                icon="cash"
+                                color="#22bd22"
+                                size={23}
+                                style={{ margin: 0, }}
+                                onPress={() => console.log('Pressed')}
+                            />
+                        </View>
+                        <Text style={styles.textInfoIcon}>Price</Text>
+                    </View>
+                    <View>
+                        <View style={styles.iconContainer}>
+                            <Text style={styles.amount}>{itinerary.duracion} </Text>
+                            <IconButton
+                                icon="clock-time-four-outline"
+                                color="rgb(72,144,226)"
+                                size={23}
+                                style={{ margin: 0, }}
+                            />
+                        </View>
+                        <Text style={styles.textInfoIcon}>Duration</Text>
+                    </View>
                 </View>
-            </View>
-            <View style={{flexDirection:'row',justifyContent:"space-around",marginTop:20,width:"80%"}}>
-                {itinerary.hashTags.map(hashTag => <Chip key={hashTag} mode="outlined" >{hashTag}</Chip>)}
-            </View>
-            <ScrollView horizontal={true} style={{marginTop:20}}>
-                {itinerary.activities.map((activity,index)=> {
-                    return (
-                        <TouchableRipple key={activity._id}  onPress = {()=>{setStateImageView({
-                            ...stateImageView,
-                            index,
-                            visible:true
-                        })}}>
-                            <Image source={{uri:activity.imagenHost}} style={styles.imagenActivity} />
-                        </TouchableRipple>
-                    )
-                        
-            })}
-                
-            </ScrollView>
-            
-            {<ImageView
-                images={images}
-                imageIndex={stateImageView.index}
-                visible={stateImageView.visible}
-                onRequestClose={() => setStateImageView({...stateImageView,visible:false})}
-            />}
+                <View style={{ flexDirection: 'row', justifyContent: "space-around", marginTop: 20, width: "80%" }}>
+                    {itinerary.hashTags.map(hashTag => <Chip key={hashTag} mode="outlined" >{hashTag}</Chip>)}
+                </View>
+
+                <View style={{ height: 200 }}>
+                    <ScrollView horizontal={true} style={{ marginTop: 20 }}>
+                        {itinerary.activities.map((activity, index) => {
+                            return (
+                                <TouchableRipple key={activity._id} onPress={() => {
+                                    setStateImageView({
+                                        ...stateImageView,
+                                        index,
+                                        visible: true
+                                    })
+                                }}>
+                                    <Image source={{ uri: activity.imagenHost }} style={styles.imagenActivity} />
+                                </TouchableRipple>
+                            )
+                        })}
+                    </ScrollView>
+                    <ImageView
+                        images={images}
+                        imageIndex={stateImageView.index}
+                        visible={stateImageView.visible}
+                        onRequestClose={() => setStateImageView({ ...stateImageView, visible: false })}
+                    />
+                </View>
+                <View style={styles.containerBoxComments}>
+                    <Text style={styles.title}>Leave a Comment</Text>
+                </View>
 
 
 
-            <Text>Hola este es Itinerary</Text>
-        </View>
+            </View>
+        </ScrollView>
     )
 
 };
 
 const styles = StyleSheet.create({
     infoItinerary: {
-        flexDirection: "row"
+        flexDirection: "row",
+        marginLeft: 30,
+        width: "100%",
+        flexWrap: "nowrap",
+        alignItems:'center'
     },
     title: {
+        width: "90%",
         fontSize: 35,
         color: "white",
     },
@@ -129,16 +143,20 @@ const styles = StyleSheet.create({
         justifyContent: 'center',
 
     },
-    scrollHorizontal:{
-        marginLeft:30,
+    scrollHorizontal: {
+        marginLeft: 30,
+        height: 200
     },
 
-    imagenActivity:{
-        width:350,
-        height:200,
-        marginHorizontal:10
+    imagenActivity: {
+        width: 350,
+        height: 200,
+        marginHorizontal: 10
+    },
+    containerBoxComments: {
+        marginTop: 20,
+        alignItems: "center"
     }
-
 })
 
 export default Itinerary;
